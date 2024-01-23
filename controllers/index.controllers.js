33bf02c4c2ff83feda139142c126f11f8b57cd87
Julian Rojas <juliandra140201@gmail.com>
2024-01-23 18:11:58 -0500

    Why?

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -2,7 +2,8 @@ import { pool } from '../index.js';
 
 export const getIndex = async (req, res) => {
   try {
-    res.send("All right")
+    res.send("All right");
+    console.log("Todo bien");
   } catch (error) {
     console.error('Error:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
