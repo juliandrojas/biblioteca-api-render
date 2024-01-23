828e80bc26077a3c7de17821f5a25d95d2764f8b
Julian Rojas <juliandra140201@gmail.com>
2024-01-23 16:31:42 -0500

    Add("The result of query")

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express();
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL
 })
-app.get('/', (req, res) => {
-  res.send("Hola Mundo")
+app.get('/', async (req, res) => {
+  const result = await pool.query("SELECT NOW()")
+  return res.json(result.rows[0]);
 })
 app.get('/ping', async (req, res) => {
   const result = await pool.query("SELECT NOW()")
