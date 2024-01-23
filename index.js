7244d33697f5f56cd0dffc7cc6f0158abb988808
Julian Rojas <juliandra140201@gmail.com>
2024-01-23 16:29:05 -0500

    Add("First test to db(Date in db)")

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+import { config } from 'dotenv';
 import express from 'express';
+import pg from 'pg';
+config()
 const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+})
 app.get('/', (req, res) => {
   res.send("Hola Mundo")
+})
+app.get('/ping', async (req, res) => {
+  const result = await pool.query("SELECT NOW()")
+  return res.json(result.rows[0]);
 })
 app.listen(3000);
 console.log('Server on port: ',3000)
